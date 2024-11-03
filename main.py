cf6c95f9dcd4b6b523f8529fd04dd5c34b07bbbc
Wince90 <bswiincent@gmail.com>
2024-11-03 11:33:52 +0100

    add asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame as py
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background_color = ("black")
@@ -13,6 +15,11 @@ def main():
 
     updatable = py.sprite.Group()
     drawable = py.sprite.Group()
+    asteroids = py.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20)
@@ -27,7 +34,7 @@ def main():
         screen.fill(background_color)
         
         for obj in drawable:
-            obj.draw(dt)
+            obj.draw(screen)
 
         py.display.flip()
         dt = time.tick(60) / 1000

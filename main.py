1be1fcdd13fe642f6800c370c165a21bebeca271
Wince90 <bswiincent@gmail.com>
2024-11-03 09:01:55 +0100

    setup screen

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import pygame
+import pygame as py
+from constants import *
+
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+background_color = ("black")
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+def game_run():
+    is_on = True
+    while is_on:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                return
+        screen.fill(background_color)
+        py.display.flip()
+
 
 if __name__ == "__main__":
     main()
+    py.init()
+    game_run()

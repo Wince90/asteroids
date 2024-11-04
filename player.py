5aae5ac7ab726237e37e8fe5c9bd4e69c1010634
Wince90 <bswiincent@gmail.com>
2024-11-04 10:14:41 +0100

    shot rate limit

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame as py
 from circleshape import CircleShape
 from shot import Shoot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.radius = PLAYER_RADIUS
         self.rotation = 0
+        self.timer = 0
 
     def draw(self, screen):
         py.draw.polygon(screen, "white", self.triangle(), 2)
@@ -26,6 +27,8 @@ class Player(CircleShape):
     def update(self, dt):
         keys = py.key.get_pressed()
 
+        self.timer -= dt
+
         if keys[py.K_a]:
             self.rotate(-dt)
         if keys[py.K_d]:
@@ -35,7 +38,8 @@ class Player(CircleShape):
         if keys[py.K_s]:
             self.move(-dt)
         if keys[py.K_SPACE]:
-            self.shoot()
+            if not(self.timer > 0):
+                self.shoot()
 
     def move(self, dt):
         forward = py.Vector2(0, 1).rotate(self.rotation)
@@ -44,4 +48,4 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shoot(self.position.x, self.position.y)
         shot.velocity = py.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-
+        self.timer = PLAYER_SHOOT_COOLDOWN
